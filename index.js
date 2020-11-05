d3e1a84d8d21ad64d257a94c80c6fc3f367e68b6
hannawaldmann <han310@rams.colostate.edu>
2020-11-05 15:45:14 -0700

    Week 10.1, 10/27 database, /movies

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const express = require('express')
+const db = require('./dbConnectExec.js')
 
 const app = express();
 
-app.get("/hi", (request,response)=>{
+//http://localhost:5000/hi
+app.get("/hi", (req,res)=>{
 response.send("hello world")
 })
 
+//http://localhost:5000/movies
+app.get("/movies", (req,res)=>{
+    //get data from database
+    db.executeQuery(`SELECT *
+    //     FROM Movie
+    //     LEFT JOIN Genre 
+    //     ON Genre.GenrePK = Movie.GenreFK`)
+    .then((result)=>{
+        res.status(200).send(result)
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.status(500).send()
+    })
+})
 
+app.get("/movies/:pk", (req,res)=>{
+    var pk = req.params.pk
+    // console.log("my PK:" + pk)
 
+    var myQuery = `SELECT *
+    FROM Movie
+    LEFT JOIN Genre
+    ON Genre.GenrePK = Movie.GenreFK
+    WHERE MoviePK = ${pk}`
+
+    db.executeQuery(myQuery)
+    .then((movies)=>{
+        // console.log("Movies: " + movies)
+        if(movies[0]){
+            res.send(movies[0])
+        }else{
+            res.status(404).send('bad request')
+        }
+    })
+    .catch((err)=>{
+        console.log("Error in /movies/pk",err)
+        res.status(500).send()
+    })
+})
 app.listen(5000,()=>{console.log("app is running on port 5000")})
 
-//http://localhost:5000/hi
